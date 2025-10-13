16cbf220789f0969a638d25f3f12922f9f2f9bdb
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2025-10-13 22:17:29 +0100

    Remove cancellation hint from plugma example to streamline user experience

--- examples/plugma.ts
+++ examples/plugma.ts
@@ -19,8 +19,6 @@ const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const flow = async () => {
 	await note("[ Plugma ]{bgMagenta} [v2.1.0]{dim}");
 
-	await note("[Press Ctrl+C to cancel]{dim}");
-
 	await completedFields();
 
 	const answers = await group(
